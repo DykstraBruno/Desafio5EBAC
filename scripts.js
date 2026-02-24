f91c6cc23f3fbb8ac0c9938467b90fda7e43368a
bruno dykstra <brunodykstra@gmail.com>
2026-02-24 18:30:32 -0300

    teste

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 const clientes = document.getElementById("clientes");
 fetch("https://crudcrud.com/api/5b1dea69f8784bc9b8b7e07c26b66660/clientes")
-
+.then(resposta=> resposta.json())
+.then((clientesCadastrados) => {
+    clientesCadastrados.array.forEach(clientes)
+})
 const nome = document.getElementById("nome");
+
 const email = document.getElementById("email");
     
-
 const listar = document.getElementById("listar");
 
 const excluir = document.getElementById("excluir");
