72403caf911ce6cbcbd56c1b011af29bca88540e
bruno dykstra <brunodykstra@gmail.com>
2026-02-24 21:01:07 -0300

    teste

--- scripts.js
+++ scripts.js
@@ -1,21 +1,14 @@
 const clientes = document.getElementById("clientes");
+
 fetch("https://crudcrud.com/api/5b1dea69f8784bc9b8b7e07c26b66660/clientes")
 .then(resposta=> resposta.json())
 .then((clientesCadastrados) => {
-    clientesCadastrados.array.forEach(clientes)
-})
 
     clientesCadastrados.forEach(clientes => {
+        
         const nome = document.getElementById("nome");
+        item.innerHTML = `${clientes.cadastrar} <button>Excluir</button>`;
+        clientes.appendChild(item);
     });
-const nome = document.getElementById("nome");
-
-const email = document.getElementById("email");
-    
-const listar = document.getElementById("listar");
-
-const excluir = document.getElementById("excluir");
-
-
-
-
+})
+  
